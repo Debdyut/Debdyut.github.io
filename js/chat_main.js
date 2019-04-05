060c9706b74643de9f205f2c8d630c13b7281adb
Debdyut Hajra <debdyuthajra@ymail.com>
2019-04-05 09:11:03 +0530

    Almost done

--- js/chat_main.js
+++ js/chat_main.js
@@ -1,4 +1,6 @@
 $("#chat_trigger").click(function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera  
     $("#chatbot_container").toggle();  
     update_msg_area_size(); 
     $("body").css("overflow", "hidden");
